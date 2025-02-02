af4acbf2bcf61653a70a5191191766f87d323b7f
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2025-02-02 22:55:38 +0800

    Add: Dogs vs. Cats (PyTorch)

---
+++ pages/dogs_vs_cats.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.info("PyTorch, torchvision, Computer Vision, Transfer Learning", icon="ℹ️")
+st.write("Watch the video below to view the Deep Learning final exam I made when I was teaching in Ateneo de Naga University.")
+
+st.video("https://youtu.be/Jj60fggvgOk")
--- pages/intro.py
+++ pages/intro.py
@@ -4,6 +4,10 @@ st.subheader("Sentiment Analysis with Hugging Face")
 st.write("Analyze reviews and comments from your users with comment analyzer.")
 st.page_link("pages/comment_analyzer.py", icon="🔎")
 
-st.subheader("Customer Segmentation - EDA and Prediction")
-st.write("Let's analyze the segmentation of a retail store. You can input values to predict its segment.")
+st.subheader("Customer Segmentation - K-Means Clustering")
+st.write("Let's analyze the segmentation of a retail store.")
 st.page_link("pages/customer_segmentation.py", icon="🔮")
+
+st.subheader("Classifying Dogs and Cats - Transfer Learning")
+st.write("Let's identify if an image is a dog or a cat. View the finals notebook I created when I was teaching Deep Learning in Ateneo de Naga University.")
+st.page_link("pages/dogs_vs_cats.py", icon="🐶")
